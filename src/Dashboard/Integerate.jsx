15a3de1ccde7907389c8770cb78bc34adb5d69d2
Nilesh Shukla <shuklanilesh810@gmail.com>
2025-10-14 23:59:11 +0530

    Updated the Dashboard Section and now moving to backend operation

--- src/Dashboard/Integerate.jsx
+++ src/Dashboard/Integerate.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import alert from "/alert.png"
 
 function Integerate() {
   return (
-    <div>Integerate</div>
+    <div className='flex flex-col justify-center items-center'>
+      <img src={alert} alt="alert sign" className='w-1/2 h-1/2 opacity-20' />
+      <h1 className='text-center text-lg text-[#789]'>This section is still under construction and will be available soon.</h1>
+    </div>
   )
 }
 
