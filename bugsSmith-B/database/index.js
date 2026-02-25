7a0dcbaf477c2f0cd4540c6bbb05edf4617b96da
Nilesh Shukla <shuklanilesh810@gmail.com>
2026-02-25 11:14:42 +0530

    Confirmed if the database is using Transaction Poller for connection or not

--- bugsSmith-B/database/index.js
+++ bugsSmith-B/database/index.js
@@ -8,6 +8,9 @@ const { Pool } = pg;
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: { rejectUnauthorized: false },
+    max:10,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 5000
 });
 
 pool.on('connect', () => {
